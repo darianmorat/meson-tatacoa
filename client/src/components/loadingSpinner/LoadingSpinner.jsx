5b74350abaacdc473c14a983681bc076ca3d2655
darianmorat <yojhandariantoledomora@gmail.com>
2025-05-05 09:39:53 -0500

    feat(loading): add loading spinner component

---
+++ client/src/components/loadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./loadingSpinner.css";
+
+export const LoadingSpinner = () => {
+   return (
+      <>
+         <div className="loader" />
+         <div className="loader-2" />
+      </>
+   );
+};
