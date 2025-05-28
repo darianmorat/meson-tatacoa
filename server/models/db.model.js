a2c89fd93df6bc88dfda473dae04ddfc759248e8
darianmorat <yojhandariantoledomora@gmail.com>
2025-05-28 14:02:41 -0500

    feat(model): add new db model

---
+++ server/models/db.model.js
@@ -0,0 +1,105 @@
+import pool from "../db/pool.js";
+
+export const sliderModel = {
+   get: async () => {
+      return pool.query(
+         ` 
+            select * from slider_imgs
+         `,
+      );
+   },
+
+   create: async (imageUrl) => {
+      return pool.query(
+         ` 
+         insert into slider_imgs 
+         (image_url) values ($1) 
+         returning *
+      `,
+         [imageUrl],
+      );
+   },
+
+   delete: async (id) => {
+      return pool.query(
+         `
+         delete from slider_imgs where 
+         id = $1 
+         returning *
+      `,
+         [id],
+      );
+   },
+};
+
+export const menuModel = {
+   get: async (categoryName) => {
+      return pool.query(
+         `
+            select * from menu_items where 
+            category_id = (
+               select id from categories where 
+               name = $1
+            )
+         `,
+         [categoryName],
+      );
+   },
+
+   category: async (categoryName) => {
+      return pool.query(
+         `
+         select id from categories where 
+         name = $1
+      `,
+         [categoryName],
+      );
+   },
+
+   edit: async (name, price, description, imageUrl, id) => {
+      return pool.query(
+         `
+         update menu_items set 
+         name = $1, 
+         price = $2, 
+         description = $3, 
+         image_url = $4 
+         where id = $5 
+         returning *
+      `,
+         [name, price, description, imageUrl, id],
+      );
+   },
+
+   create: async (categoryId, name, price, description, imageUrl) => {
+      return pool.query(
+         `
+         insert into menu_items (
+            category_id, 
+            name, 
+            price, 
+            description, 
+            image_url
+         ) 
+         values (
+            $1, 
+            $2, 
+            $3, 
+            $4, 
+            $5
+         )
+      `,
+         [categoryId, name, price, description, imageUrl],
+      );
+   },
+
+   delete: async (id) => {
+      return pool.query(
+         `
+         delete from menu_items 
+         where id = $1
+      `,
+         [id],
+      );
+   },
+};
